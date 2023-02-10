b60b7fe435fbe5a33578a090ddbae66673251ce8
Bhupathi2.0 <92458357+Bhupathi20@users.noreply.github.com>
2023-02-10 22:36:28 +0530

    Add files via upload

---
+++ DeSerialization/User.java
@@ -0,0 +1,13 @@
+package DeSerialization;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    
+    String name;
+    transient String password;
+
+    public void sayHello(){
+        System.out.println("Hello " + name);
+    }
+}
